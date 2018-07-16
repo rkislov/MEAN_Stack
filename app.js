3c8754154bb58f8d3eae32ee9a76f1fee4795af8
Roman@kislovs.ru <roman@kislovs.ru>
2018-07-16 13:47:09 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const authRoutes = require('./routes/auth')
+const analyticsRoutes = require('./routes/analytic')
+const categoryRoutes = require('./routes/category')
+const orderRoutes = require('./routes/order')
+const positionRoutes = require('./routes/position')
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/analytics', analyticsRoutes)
+app.use('/api/category', categoryRoutes)
+app.use('/api/order', orderRoutes)
+app.use('/api/position', positionRoutes)
+
+
+module.exports = app
---
+++ controllers/position.js
@@ -0,0 +1,12 @@
+module.exports.getByCategoryId = (req,res) => {
+
+}
+module.exports.create = (req,res) => {
+
+}
+module.exports.remove = (req,res) => {
+
+}
+module.exports.update = (req,res) => {
+
+}
