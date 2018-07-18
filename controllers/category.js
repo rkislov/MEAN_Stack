a858d9e42b06ef689f11732417cf6e07c6cfc4db
Roman Kislov <roman@kislovs.ru>
2018-07-18 11:47:42 +0500

    controller and uplod files

--- controllers/category.js
+++ controllers/category.js
@@ -1,17 +1,65 @@
-module.exports.getAll = (req,res) => {
-    res.json({message: 'Categories'})
-}
-module.exports.getById = (req,res) => {
+const Category = require('../models/Category')
+const Position = require('../models/Position')
+const errorHandler = require('../utils/errorHandler')
 
+module.exports.getAll = async (req,res) => {
+    try {
+        const categories = await new Category.find({
+            user: req.user.id
+        })
+        res.status(200).json(categories)
+    } catch (e) {
+        errorHandler(res,e)
+    }
 }
-module.exports.remove = (req,res) => {
-
+module.exports.getById = async (req,res) => {
+    try {
+        const category = await Category.findById(req.params.id)
+        res.status(200).json(category)
+    } catch (e) {
+        errorHandler(res,e)
+    }
 }
-module.exports.create = (req,res) => {
-
+module.exports.remove = async (req,res) => {
+    try {
+        await Category.remove({_id: req.params.id})
+        await Position.remove({category: req.params.id})
+        res.status(200).json({
+            message: `Категория удалена`
+        })
+    } catch (e) {
+        errorHandler(res,e)
+    }
 }
-module.exports.update = (req,res) => {
-
+module.exports.create = async (req,res) => {
+    const category = new Category({
+        name: req.body.name,
+        user: req.user.id,
+        imageSrc: req.file ? req.file.path : ''
+    })
+    try {
+        await category.save()
+        res.status(201).json(category)
+    } catch (e) {
+        errorHandler(res,e)
+    }
 }
-
+module.exports.update = async (req,res) => {
+    const updated = {
+        name: req.body.name,
+    }
+    if (req.file) {
+        updated.imageSrc = req.file.path
+    }
+    try {
+        const category = await Category.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: updates},
+            {new: true}
+        )
+        res.status(200).json(category)
+    } catch (e) {
+        errorHandler(res,e)
+    }
+}
 
--- routes/analytic.js
+++ routes/analytic.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/analytic')
+const passport = require('passport')
 
 
-router.get('/overview', controller.overview)
-router.get('/analytics', controller.analytic)
+router.get('/overview', passport.authenticate('jwt', {session: false} ),controller.overview)
+router.get('/analytics', passport.authenticate('jwt', {session: false} ),controller.analytic)
 
 
 module.exports = router
